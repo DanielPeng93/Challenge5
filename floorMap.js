fe9b5bd599670d2be4eb281d9bfa1d46c550f923
wreevehicks <home@Reeves-iMac.local>
2015-11-09 00:56:54 -0500

    Working map display in ascii
    Note: Vehicle will be displayed if location entered is in the hallway.
    If it is not in the hallway it will not return the vehicle location -
    this is so that the vehicle is not reported to be inside a wall etc.
    
    Script only needs to be fed an xy location to report on map, these
    values correspond directly to the location values we recorded today.

---
+++ floorMap.js
@@ -0,0 +1,49 @@
+var vehicleLocation = [3 ,18];
+var hallwayLocation = [0, 18, 28, 46]
+var floorMap = [];
+var row;
+var str;
+var num;
+
+for (var i = 0; i < 47; i++){
+	floorMap.push('|    |');
+}
+floorMap[hallwayLocation[0]] = '|----|';
+floorMap[hallwayLocation[1]] = '|----|';
+floorMap[hallwayLocation[2]] = '|----|';
+floorMap[hallwayLocation[3]] = '|----|';
+floorMap[48] = '012345'
+
+var count = -1;
+floorMap.map(function (item){
+	count++ 
+	for (var i = 0, len = item.length, str = '', row = ''; i < len; i++){
+		row = 46;
+		str = str + item[i] + ' ';
+	};
+	mapDisplay(row, count, str);
+	
+});
+
+function mapDisplay(row, count, str) {
+		num = row - count;
+		var numStr;
+		numStr = num.toString();
+		if (num < 10) numStr = num.toString() + ' ';
+		if (num < 0) numStr = '  ';
+		var check = atRow(hallwayLocation, num);
+		if (num === vehicleLocation[1] && check === true) {
+			console.log(numStr + '    ' + str.substring(0, vehicleLocation[0] * 2) + 'X' + str.substring(vehicleLocation[0] * 2 + 1, str.length));
+		} else {
+			console.log(numStr + '    ' + str);
+		};
+	};
+
+function atRow(hallwayLocation, num) {
+	for (var i = 0; i < hallwayLocation.length; i++){
+		if (hallwayLocation[i] === num) {
+			return true;
+			break;
+		} 
+	} return false;
+}
